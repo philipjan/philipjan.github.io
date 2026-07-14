473ed783cfe6a28066ae68e5a39b01c3ddd52db9
philip <philipjanbaruis@gmail.com>
2026-07-15 01:58:13 +0800

    portfolio-july-8-2026 - fix portfolio image not showing inside github pages

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const siteConfig = {
         "One concise email. Five minutes. Every Tuesday. Essential AI news & trends, production-ready libraries, powerful AI tools, and real-world code examples",
       link: "https://aidevroundup.com/?ref=devportfolio",
       skills: ["React", "Node.js", "AWS"],
-      images: ["/sample1.jpeg", "/sample1.jpeg"],
+      images: ["sample1.jpeg", "sample1.jpeg"],
     },
     {
       name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
@@ -27,7 +27,7 @@ export const siteConfig = {
         "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
       link: "https://fullstackextensions.com/?ref=devportfolio",
       skills: ["React", "Node.js", "AWS"],
-      images: ["/sample1.jpeg"],
+      images: ["sample1.jpeg"],
     },
     {
       name: "ExtensionKit",
@@ -35,7 +35,7 @@ export const siteConfig = {
         "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
       link: "https://extensionkit.io/?ref=devportfolio",
       skills: ["React", "Node.js", "AWS"],
-      images: ["/sample1.jpeg", "/sample1.jpeg", "/sample1.jpeg"],
+      images: ["sample1.jpeg", "sample1.jpeg", "sample1.jpeg"],
     },
   ],
   experience: [
